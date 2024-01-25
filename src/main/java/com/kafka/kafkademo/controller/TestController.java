ee048cba0de3e96edb41dd2cd2541119d4c13001
[NTT DATA]Reverson Batista Mendes <reverson.batistamendes@nttdata.com>
2024-01-25 13:59:57 -0300

    first commit

---
+++ src/main/java/com/kafka/kafkademo/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.kafka.kafkademo.controller;
+
+import com.kafka.kafkademo.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    MessageProducer messageProducer;
+
+    @PostMapping("/sendMessage")
+    public String sendMessage(@RequestParam("message") String message){
+//        messageProducer.sendMessage("ytube-topic", message);
+        return "Message as been sent successfully" + message;
+    }
+
+    @GetMapping("/teste")
+    public String getTeste(){
+        return "AAAAAAAAAAAAAAAAAAHHHHHHHHHHHHH";
+    }
+}
